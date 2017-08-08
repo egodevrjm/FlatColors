dfb2b31d17ba4acb57b91eb2db9bef4f0bcd45f7
Ryan Morrison <upyourego@icloud.com>
2017-08-08 08:44:42 +0100

    Simple flat colors
    A swift file setting out constants for the flat colors listed on the FlatUIColors site.

---
+++ flatColors.swift
@@ -0,0 +1,44 @@
+//
+//  flatColors.swift
+//  SnippetFile
+//
+//  Created by Ryan Morrison on 08/08/2017.
+//  Copyright © 2017 egoDev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// see https://flatuicolors.com for examples
+
+// green
+let flatTurquoise = UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1.0)
+let flatEmerald = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0)
+let flatGreenSea = UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0)
+let flatNephritis = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0)
+
+// blue
+let flatPeterRiver = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+let flat = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+
+// purple
+let flatAmethyst = UIColor(red: 155/255, green: 89/255, blue: 182/255, alpha: 1.0)
+let flatWisteria = UIColor(red: 143/255, green: 68/255, blue: 173/255, alpha: 1.0)
+
+// dark gray
+let flatWetAsphalt = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0)
+let flat = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+
+// light gray
+let flatConcrete = UIColor(red: 149/255, green: 165/255, blue: 166/255, alpha: 1.0)
+let flatAsbestos = UIColor(red: 127/255, green: 140/255, blue: 141/255, alpha: 1.0)
+let flatClouds = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0)
+let flatSilver = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1.0)
+
+// orange and yellow
+let flatAlizarin = UIColor(red: 231/255, green: 76/255, blue: 69/255, alpha: 1.0)
+let flatPomegranate = UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0)
+let flatCarrot = UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1.0)
+let flatPumpkin = UIColor(red: 211/255, green: 84/255, blue: 0/255, alpha: 1.0)
+let flatSunFlower = UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0)
+let flatOrange = UIColor(red: 243/255, green: 156/255, blue: 18/255, alpha: 1.0)
